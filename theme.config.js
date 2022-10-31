07e4372df3ec4cfad259b3f992ee252ecddbf857
Ivo Garofalo <ivo.garofalo@gmail.com>
2022-10-31 17:28:03 +0000

    git init

--- theme.config.js
+++ theme.config.js
@@ -1,17 +1,19 @@
+import React from "react";
+
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/Centaurifly/ethereum_repository',
+  docsRepositoryBase: 'https://github.com/Dadogg80/centaurify_readthedocs',
+  titleSuffix: ' – Centaurify - Read The Docs',
   logo: (
-    <>
+    <React.Fragment>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         The Next.js Static Site Generator
       </span>
-    </>
+      </React.Fragment>
   ),
-  head: (
-    <>
+  script: (
+    <React.Fragment>
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -55,7 +57,7 @@ export default {
         href="/favicon-16x16.png"
       />
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-    </>
+      </ React.Fragment>
   ),
   search: true,
   prevLinks: true,
